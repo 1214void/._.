165192b646a5350b2dc4ed2b0004038ca5e50b81
1214void <1214void@gmail.com>
2026-09-19 19:05:59 +0900

    Adjust TabHighlight position and size properties

--- lib.lua
+++ lib.lua
@@ -3081,8 +3081,8 @@ function Library:CreateWindow(...)
         local TabHighlight = Library:Create('Frame', {
             BackgroundColor3 = Library.AccentColor;
             BorderSizePixel = 0;
-            Position = UDim2.new(0, 0, 0, 0);
-            Size = UDim2.new(1, 0, 0, 2);
+            Position = UDim2.new(0, -1, 0, -1);
+            Size = UDim2.new(1, 2, 0, 2);
             Visible = false;
             ZIndex = 2;
             Parent = TabButton;
